ec4aba960ab3303361498984aaec5456a603e4b4
Vladimir Tinica <vltinica@gmail.com>
2024-08-04 12:00:20 -0400

    add components winning-combination  and GameOver

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,48 @@ import React, { useState } from "react";
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 import Log from "./components/Log";
+import { WINNING_COMBINATIONS } from "./components/winning-combinations";
+import GameOver from "./components/GameOver";
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
 
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
   const [activePlayer, setActivePlayer] = useState("X");
+  // const [hasWinner, setHasWinner] = useState(false)
+
+let gameBoard = initialGameBoard.map((row) => [...row]);
+
+  for (const turn of gameTurns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+    gameBoard[row][col] = player;
+  }
+
+  let winner;
+
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSquareSymbol =
+      gameBoard[combination[0].row][combination[0].column];
+    const secondtSquareSymbol =
+      gameBoard[combination[1].row][combination[1].column];
+    const thirdSquareSymbol =
+      gameBoard[combination[2].row][combination[2].column];
+
+    if (
+      firstSquareSymbol &&
+      firstSquareSymbol === secondtSquareSymbol &&
+      firstSquareSymbol === thirdSquareSymbol
+    ) {
+      winner = firstSquareSymbol;
+    }
+  }
+
+  const hasDraw = gameTurns.length === 9 && !winner;
 
   const handleSelectSquare = (rowIndex, colIndex) => {
     setGameTurns((prevTurns) => {
@@ -22,22 +60,37 @@ function App() {
       ];
 
       // Переключение игрока
-      setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "0" : "X"));
+      setActivePlayer((curActivePlayer) =>
+        curActivePlayer === "X" ? "0" : "X"
+      );
       return updatedTurns;
     });
   };
+
+  function handleRestart() {
+    setGameTurns([])
+  }
 
   return (
     <>
       <main>
         <div id="game-container">
+          {(winner || hasDraw )&& <GameOver winner={winner} onRestart={handleRestart}/>}
           <ol id="players" className="highlight-player">
-            <Player initialName="Player 1" symbol="X" isActive={activePlayer === "X"} />
-            <Player initialName="Player 2" symbol="0" isActive={activePlayer === "0"} />
+            <Player
+              initialName="Player 1"
+              symbol="X"
+              isActive={activePlayer === "X"}
+            />
+            <Player
+              initialName="Player 2"
+              symbol="0"
+              isActive={activePlayer === "0"}
+            />
           </ol>
-          <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
+          <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
         </div>
-        <Log  turns={gameTurns}  />
+        <Log turns={gameTurns} />
       </main>
     </>
   );
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,29 +1,22 @@
 import React from "react";
 
-const initialGameBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-];
 
-const GameBoard = ({ onSelectSquare, turns }) => {
-  const gameBoard = initialGameBoard.map((row) => [...row]);
 
-  for (const turn of turns) {
-    const { square, player } = turn;
-    const { row, col } = square;
-    gameBoard[row][col] = player;
-  }
+const GameBoard = ({ onSelectSquare, board }) => {
+ 
 
   return (
     <div>
       <ol id="game-board">
-        {gameBoard.map((row, rowIndex) => (
+        {board.map((row, rowIndex) => (
           <li key={rowIndex}>
             <ol>
               {row.map((playerSymbol, colIndex) => (
                 <li key={colIndex}>
-                  <button onClick={() => onSelectSquare(rowIndex, colIndex)}>
+                  <button 
+                    onClick={() => onSelectSquare(rowIndex, colIndex)}
+                    disabled={playerSymbol !==null}
+                    >
                     {playerSymbol}
                   </button>
                 </li>
